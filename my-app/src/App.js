3684df2d2ce4dddb9522232dd42d6a79ebd14976
HanCai98 <han_cai1@brown.edu>
2022-07-03 15:28:07 -0400

    configure the router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,31 @@
 import './App.css';
 
-const tahoe_peaks = [
-  {name : "Freel", elevation: 10891},
-  {name : "Monument", elevation: 10067},
-  {name : "Pyramid", elevation: 9983},
-  {name : "Tallac", elevation: 9735}
-];
-
-function List({data, renderItem, renderEmpty}) {
-  return !data.length ? (renderEmpty) : (
-  <ul>
-    {data.map(item => (
-      <li key = {item.name}>
-        {renderItem(item)}
-      </li>
-    ))}
-  </ul>);
-}
-
-function App() {
+function Home() {
   return (
-    <List 
-      data = {tahoe_peaks}
-      renderEmpty = {<p>This list is empty</p>}
-      renderItem = {item =>
-        <>
-          {item.name} - {item.elevation} ft.
-        </>}
-    />
+    <div>
+      <h1>My Website</h1>
+    </div>
   );
 }
 
-export default App;
+export function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+    </div>
+  );
+}
+
+export function Contact() {
+  return (
+    <div>
+      <h1>Contact us</h1>
+    </div>
+  );
+}
+
+export function App() {
+  return <Home />;
+}
+
 

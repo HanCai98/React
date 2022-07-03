99d65414204efd258417057d9145c0f3d98d01c0
HanCai98 <han_cai1@brown.edu>
2022-07-02 23:00:26 -0400

    fetch data with Hooks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,20 @@
 import './App.css';
-import { useState } from 'react';
-
-function useInput(initialValue) {
-  const [value, setValue] = useState(initialValue);
-  return [
-    {
-      value,
-      onChange: e => setValue(e.target.value)
-    },
-    () => setValue(initialValue)
-  ];
-}
+import { useState, useEffect} from 'react';
 
 function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] = useInput("#000000");
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch( `https://api.github.com/users/HanCai98`
+    ).then(response => response.json())
+    .then(setData);
+  }, []);
 
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${titleProps.value}, ${colorProps.value}`);
-    resetTitle();
-    resetColor();
-  };
+  if(data) return <pre>{JSON.stringify(data, null, 2)}</pre>
 
   return (
-   <form onSubmit={submit}>
-    <input
-      {... titleProps}
-      type = "text"
-      placeholder = 'color title...'
-    />
-    <input 
-      {... colorProps}
-      type = "color"
-    />
-    <button>Add</button>
-   </form>
+   <h1>
+    Data
+   </h1>
   );
 }
 

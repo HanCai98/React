f1fda0e3de26c2363445b66a7f74376554d39399
HanCai98 <han_cai1@brown.edu>
2022-07-03 16:38:10 -0400

    test events

---
+++ my-app/src/Checkbox.test.js
@@ -0,0 +1,10 @@
+import {fireEvent, getAllByLabelText, render} from "@testing-library/react";
+import {Checkbox} from "./Checkbox";
+
+test("Selecting checkbox should change value to true", () => {
+    const {getByLabelText} = render(<Checkbox />);
+    const checkbox = getByLabelText(/not checked/i);
+
+    fireEvent.click(checkbox);
+    expect(checkbox.checked).toEqual(true);
+});

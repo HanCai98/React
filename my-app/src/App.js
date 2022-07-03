2c8401716c7ca1030965c10f76f966bff1ab2dde
HanCai98 <han_cai1@brown.edu>
2022-07-02 23:23:08 -0400

    handle loading states

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,20 +13,31 @@ function GitHubUser({name, location, avatar}) {
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     fetch( `https://api.github.com/users/HanCai98`
     ).then(response => response.json())
-    .then(setData);
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError)
   }, []);
 
-  if(data) return (
-    <GitHubUser name={data.name} location={data.location} avatar={data.avatar_url}/>
-  );
+  if (loading) return <h1>Loading ...</h1>;
+
+  if (error)
+    return <pre>{JSON.stringify(error)}</pre>;
+  
+  if (!data) return null;
 
   return (
-   <h1>
-    Data
-   </h1>
+    <GitHubUser 
+      name={data.name} 
+      location={data.location} 
+      avatar={data.avatar_url}
+    />
   );
 }
 
